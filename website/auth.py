1d42558ea3c6aa3c52ce3924c76a2af242822e9d
Newbie2008 <veerrohitv08@gmail.com>
2021-02-03 14:44:30 -0800

    had to fricking do extra stuff cuz of fricking sarvesh

--- website/auth.py
+++ website/auth.py
@@ -3,15 +3,18 @@ from flask import Blueprint, render_template, request, flash
 # This file is the blueprint for the website
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    #data = request.form
+    # print(data)
     return render_template("login.html", boolean=False)
+
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -32,5 +35,5 @@ def sign_up():
         else:
             # add user to database
             flash("Your account was created successfully!", category='success')
-            
+
     return render_template("sign_up.html")
